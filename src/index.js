34419d9cc335159f15c847c98085b316c93be086
nguyenhuudungz <nguyenhuudungz@yahoo.com>
2021-12-08 17:13:24 +0700

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Helmet } from "react-helmet";
+
+function HomePage() {
+  return (
+    <div>
+      <Helmet>
+        <title>React App - HomePage</title>
+        <meta name="twitter:title" content="React App - HomePage" />
+      </Helmet>
+      <div>HomePage</div>
+    </div>
+  );
+}
+
+function AboutPage() {
+  return (
+    <div>
+      <Helmet>
+        <title>React App - AboutPage</title>
+        <meta name="twitter:title" content="React App - AboutPage" />
+      </Helmet>
+      <div>AboutPage</div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
